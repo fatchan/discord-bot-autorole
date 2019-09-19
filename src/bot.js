ae0c62b7e2084e99ea28da059cda4d29a422c22b
fatchan <tom@69420.me>
2019-09-19 07:31:41 +0000

    spa branch with autorole, autonick and channel wiping daily

--- src/bot.js
+++ src/bot.js
@@ -1,17 +1,33 @@
 'use strict';
 
 const config = require('./config.json')
-	, Eris = require('eris');
-
-const bot = new Eris(config.token);
+	, Eris = require('eris')
+	, bot = new Eris(config.token)
+	, rr = (min, max) => Math.floor(Math.random() * (max-min + 1) + min)
+	, vwNums = [ '０', '１', '２', '３', '４', '５', '６', '７', '８', '９' ]
+	, vwConv = (num) => String(num).split('').reduce((acc, curr) => `${acc}${vwNums[+curr]}`, '');
 
 bot.on('ready', () => {
     console.log('Ready!');
 	bot.editStatus('invisible', {});
+	bot.editNickname(config.guildID, '００００');
+	const guild = bot.guilds.get(config.guildID);
+	setInterval(() => {
+		const textChannels = guild.channels.filter(c => c.type === 0);
+		const names = textChannels.map(c => c.name);
+		textChannels.forEach(c => {
+			c.delete('Deleted on 24 hour schedule by Tom\'s Bot');
+		});
+		names.forEach(n => {
+			guild.createChannel(n, 0, 'Recreated channel by Tom\'s Bot', config.categoryID);
+		});
+	}, 86400000);
 });
 
 bot.on('guildMemberAdd', (guild, member) => {
-	guild.addMemberRole(member.id, config.roleID, `Role added by Tom's AutoRole`)
+	guild.addMemberRole(member.id, config.roleID, 'Role added by Tom\'s Bot');
+	const randNum = rr(1000, 9999);
+	member.edit({ nick: config.vw ? vwConv(randNum) : randNum }, 'Nickname set by Tom\'s Bot');
 });
 
 bot.connect();
